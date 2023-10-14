6d15b141d3a26ec0b475ab8d038fe4bb6275eb44
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-10-14 20:22:01 +0530

    the file has been updated

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 var username:string = "john";
-// document.write("hello"+username);
-console.log("hello " + username);
+// document.write("hello"+username);//this print ststement will show in the browser when we run the html file.
+console.log("hello " + username);//this print ststement will not show in the brwser because log() is a brwser mwthod
