b7147a06f94871e7b5ac94c64458dbd8a318cfca
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-10-14 21:03:49 +0530

    the file has been updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-var username = "john";
-// document.write("hello"+username);
-console.log("hello " + username);
+// var username:string = "john";
+// document.write("hello"+username);//this print ststement will show in the browser when we run the html file.
+// console.log("hello " + username);//this print ststement will not show in the brwser because log() is a brwser mwthod
+function demofirst() {
+    var x = 10;
+    if (x == 10) {
+        var y = 20;
+    }
+    console.log("x=" + "\n" + "y=");
+}
+demofirst();
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-var username:string = "john";
+// var username:string = "john";
 // document.write("hello"+username);//this print ststement will show in the browser when we run the html file.
-console.log("hello " + username);//this print ststement will not show in the brwser because log() is a brwser mwthod
+// console.log("hello " + username);//this print ststement will not show in the brwser because log() is a brwser mwthod
+function demofirst()
+{
+    var x = 10;
+    if(x==10)
+    {
+        var y = 20;
+    }
+    console.log("x="+x+"\n"+"y=");
+    
+}
+demofirst()
