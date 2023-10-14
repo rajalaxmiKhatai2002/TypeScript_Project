5f8ed1434e76e8fac1009693b3507ed06ac2446a
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-10-14 21:28:01 +0530

    the file has been updated

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@
 // console.log("hello " + username);//this print ststement will not show in the brwser because log() is a brwser mwthod
 function demofirst()
 {
-    var x = 10;
-    if(x==10)
-    {
-        var y = 20;
-    }
-    console.log("x="+x+"\n"+"y=");
+    // var x = 10;
+    // if(x==10)
+    // {
+    //     var y = 20;
+    // }
+    // console.log("x="+x+"\n"+"y=");
+    
     
 }
 demofirst()
