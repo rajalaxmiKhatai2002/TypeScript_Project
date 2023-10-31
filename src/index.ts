d2daed1aa5f2002b32dba416a4760b177b5d4a57
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-10-31 20:23:56 +0530

    the file has been updated

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 // var username:string = "john";
 // document.write("hello"+username);//this print ststement will show in the browser when we run the html file.
 // console.log("hello " + username);//this print ststement will not show in the brwser because log() is a brwser mwthod
-function demofirst()
-{
-    // var x = 10;
-    // if(x==10)
-    // {
-    //     var y = 20;
-    // }
-    // console.log("x="+x+"\n"+"y=");
+// function demofirst()
+// {
+//     // var x = 10;
+//     // if(x==10)
+//     // {
+//     //     var y = 20;
+//     // }
+//     // console.log("x="+x+"\n"+"y=");
     
     
-}
-demofirst()
+// }
+// demofirst()
+var x = 1;
+x = 34;  
