907feb076955d1f75a84946e97bf7c46cc423a7d
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-10-31 21:32:05 +0530

    the file has been updated

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@
 // demofirst()
 // var x = 1;
 // x = 34;  
-var bit = 21;
-var exp = 2e3;
-console.log("bit=" + bit + "\n" + "exponent=" + exp);
+// const bit:number = 0b10101;
+// const exp:number = 2e3;
+// console.log("bit=" + bit + "\n" + "exponent=" + exp);
+var username = "john";
+var age = 23;
+var msg = "hello !" + username + "you will be" + (age + 1) + "next year"; //this is actually the old version of js 
+var ms2 = "hello ! ".concat(username, " you will be ").concat(age + 1, " next year"); //this is the moder js to print the inding statement in string
+console.log(msg);
+console.log(ms2);
